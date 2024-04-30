a1f5ff30424cbe7722612ec19957a1e1cb41d47f
Rubén Waldo <ruben.waldo6703@alumnos.udg.mx>
2024-04-30 09:33:52 -0600

    Actualización al menú

--- menu.cpp
+++ menu.cpp
@@ -43,7 +43,20 @@ void newDirectory(Directory* actualDir, string userAnswer,NodeType type){
 }
 
 void moveDirectory(Directory* actualDir, string userAnswer){
-    actualDir->setCurrentDirectory(userAnswer);
+    if (userAnswer == "..") { // Retroceder al directorio padre
+        actualDir->goToParentDirectory();
+    } else if (userAnswer == "/") { // Ir al directorio raíz
+        actualDir->setCurrentDirectory("/");
+    } else {
+        FileNode* destination = actualDir->findNode(userAnswer); // Buscar el destino, implementé esto porque se podía acceder a un archivo como directorio y de esta forma se evita este error.
+
+        if (destination->getType() == NodeType::File) {
+            cout<<"No se puede acceder a un archivo como directorio."<<enl<< endl;
+            return;
+        }
+
+        actualDir->setCurrentDirectory(userAnswer);
+    }
     cls();
 }
 
@@ -53,11 +66,13 @@ void menu(){
 
     while (userAnswer != "exit"){
         cout<<"Bienvenido al Sistema de Archivos Virtuales"<<enl;
-        cout<<"Dirrectorio Actual: "<<dir.getCurrentDirectory()->getName()<<enl;
+        cout<<"Directorio Actual: "<<dir.getCurrentDirectory()->getName()<<enl;
         cout<<"Commandos Disponibles: "<<enl;
         cout<<"* dir + (Directorio) -> Crea un Nuevo directorio."<<enl;
         cout<<"* touch + (Archivo)  -> Crea un Nuevo Archivo."<<enl;
         cout<<"* cd + (Directorio)  -> Cambia de directorio."<<enl;
+        cout<<"* cd ..              -> Directorio anterior."<<enl;
+        cout<<"* cd /               -> Directorio raiz."<<enl;
         cout<<"* ls                 -> Lista del Contenido."<<enl;
         cout<<"* exit               -> Termina el Programa."<<enl;
         cout<<enl<<enl;
@@ -65,27 +80,41 @@ void menu(){
         cout<<"~/"<<dir.getCurrentDirectory()->getName()<<": ";
         getline(cin,userAnswer);
         auto vi_userAnswer = explode(userAnswer, ' ');
+        cls();
         if(vi_userAnswer.size() <= 2){
             if(vi_userAnswer[0] == "dir"){  // logica para crear directorio
-                newDirectory(&dir,vi_userAnswer[1], NodeType::Directory);
-                cout<<"Directorio Creado con exito!"<<enl<<enl;
+                if(vi_userAnswer.size() == 2) { //Agregué estás líneas, "if(vi_userAnswer.size() == 2)", dentro de las respuestas del usuario para evitar que se cerrara la aplicación
+                    newDirectory(&dir, vi_userAnswer[1], NodeType::Directory);
+                    cout << "Directorio Creado con exito!" << enl << enl;
+                } else {
+                    cout << "Faltan parametros!" << enl;
+                }
             }
             else if(vi_userAnswer[0] == "touch"){ // Logica para crear Archivo
-                newDirectory(&dir,vi_userAnswer[1], NodeType::File);
-                cout<<"Archivo Creado con exito!"<<enl<<enl;
+                if(vi_userAnswer.size() == 2) {
+                    newDirectory(&dir, vi_userAnswer[1], NodeType::File);
+                    cout << "Archivo Creado con exito!" << enl << enl;
+                } else {
+                    cout << "Faltan parametros!" << enl;
+                }
             }
             else if(vi_userAnswer[0] == "cd"){ // Logica para Moverse de directorio.
-                moveDirectory(&dir,vi_userAnswer[1]);
+                if(vi_userAnswer.size() == 2) {
+                    moveDirectory(&dir, vi_userAnswer[1]);
+                } else {
+                    cout << "Faltan parametros!" << enl;
+                }
             }
-            else if(vi_userAnswer[0] == "ls"){ // PENSAR UNA FORMA MEJOR DE MOSTRAR TODOS LOS DIRECTORIOS!
+            else if(vi_userAnswer[0] == "ls"){ // Logica para mostrar los directorios y archivos
                 cls();
-                cout<<"TODO:" <<enl;dir.getAll();
+                cout<<enl;dir.getAll();
+                cout<<enl;
             }
             else if(vi_userAnswer[0] == "exit"){
                 return;
             }
             else{
-                cout<<"Commando Desconocido!"<<enl;
+                cout<<"Comando Desconocido!"<<enl;
             }
         }
         else{
